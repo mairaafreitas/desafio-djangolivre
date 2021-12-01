e6af263418957acba1cfde10323178eeeeca0ded
JACK <jaquecdp@gmail.com>
2021-12-01 20:19:08 -0300

    Criando serializer e views do modulo transferencia

--- djangolivre/urls.py
+++ djangolivre/urls.py
@@ -1,4 +1,6 @@
 from conta.views import ContaViewSet
+from transferencia.models import Transferencia
+from transferencia.views import TransferenciaViewSet
 from usuario.views import UsuarioViewSet
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -8,6 +10,7 @@ router = DefaultRouter()
 
 router.register(r'conta', ContaViewSet)
 router.register(r'usuario', UsuarioViewSet)
+router.register(r'transferencia', TransferenciaViewSet)
 
 urlpatterns = [
 path('', include(router.urls)),
---
+++ transferencia/serializer.py
@@ -0,0 +1,7 @@
+from .models import Transferencia
+from rest_framework import serializers
+
+class TransferenciaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transferencia
+        fields = ['remetente', 'favorecido', 'valor', 'data']
