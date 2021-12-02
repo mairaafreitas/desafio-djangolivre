ca4d4a434f765ecc7402de274a2f825e3006d355
Andreia Baumgartner <baumandreia@hotmail.com>
2021-12-01 23:17:41 -0300

    Criando endpoind de transferência de aplicação

--- djangolivre/urls.py
+++ djangolivre/urls.py
@@ -1,6 +1,6 @@
 from conta.views import ContaViewSet
 from transferencia.models import Transferencia
-from transferencia.views import TransferenciaViewSet
+from transferencia.views import TransferenciaViewSet, executa_transferencia
 from usuario.views import UsuarioViewSet
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -13,7 +13,8 @@ router.register(r'usuario', UsuarioViewSet)
 router.register(r'transferencia', TransferenciaViewSet)
 
 urlpatterns = [
-path('', include(router.urls)),
-path('admin/', admin.site.urls)
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('transferencias/', executa_transferencia)
 ]
 
--- transferencia/views.py
+++ transferencia/views.py
@@ -1,9 +1,48 @@
 from rest_framework import serializers, viewsets
 from transferencia.models import Transferencia 
 from transferencia.serializer import TransferenciaSerializer
+from rest_framework.decorators import api_view
+from conta.models import Conta
+from usuario.models import Usuario
+from usuario.serializer import UsuarioSerializer
 
 class TransferenciaViewSet(viewsets.ModelViewSet):
     queryset = Transferencia.objects.all()
     serializer_class = TransferenciaSerializer
+
+@api_view(['POST'])
+def executa_transferencia(request):
+    print(request.data)    
+    transf = Transferencia()
+    transf.valor
+
+    remetente = Usuario.objects.get(conta=request.data.get('remetente'))
+    favorecido = Usuario.objects.get(conta=request.data.get('favorecido'))
+    valor_transferencia = request.data.get('valor')
+    print("Carteira do cliente: " + str(remetente.saldo))
+    print("Valor da : " + str(favorecido.saldo))
+
+    if remetente.saldo >= valor_transferencia:
+        remetente.saldo -= valor_transferencia  
+        favorecido.saldo += valor_transferencias
+        
+        usuario_remetente = UsuarioSerializer(data=remetente)
+        if usuario_remetente.is_valid():
+            usuario_remetente.save()
+
+        usuario_favorecido = UsuarioSerializer(data=favorecido)
+        if usuario_favorecido.is_valid():
+            usuario_favorecido.save()
+
+        serializer = TransferenciaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+        print("Fez Transferência")
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)   
+
+    return Response("Cliente não possui dinheiro suficiente para a transferência", status=status.HTTP_412_PRECONDITION_FAILED)    
+
 
 
